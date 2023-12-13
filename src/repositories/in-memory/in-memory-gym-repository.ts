8a08c83bd42585ecaa314b566d1f26989c0f0b31
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-12-13 08:50:41 -0300

    feat: implement gym dependency on check in

---
+++ src/repositories/in-memory/in-memory-gym-repository.ts
@@ -0,0 +1,14 @@
+import { GymRepository } from '../gym-repository'
+import { Gym } from '@prisma/client'
+
+export class InMemoryGymRepository implements GymRepository {
+  public items: Gym[] = []
+
+  async findById(id: string) {
+    const gym = this.items.find((item) => item.id === id)
+
+    if (!gym) return null
+
+    return gym
+  }
+}
--- src/use-cases/check-in.test.ts
+++ src/use-cases/check-in.test.ts
@@ -1,21 +1,84 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from './check-in'
+import { InMemoryGymRepository } from '@/repositories/in-memory/in-memory-gym-repository'
+import { Decimal } from '@prisma/client/runtime/library'
 
 let checkInRepository: InMemoryCheckInsRepository
+let gymRepository: InMemoryGymRepository
 // authenticateUseCase: name the main entity or variable we are testing as sut (Sistem under Test) - Desing pattern
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInRepository)
+    gymRepository = new InMemoryGymRepository()
+    sut = new CheckInUseCase(checkInRepository, gymRepository)
+
+    gymRepository.items.push({
+      id: 'gym-01',
+      title: 'JS Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    })
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: -45.70522,
+      userLongitude: -128.40561,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -45.70522,
+      userLongitude: -128.40561,
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+        userLatitude: -45.70522,
+        userLongitude: -128.40561,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -45.70522,
+      userLongitude: -128.40561,
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -45.70522,
+      userLongitude: -128.40561,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
