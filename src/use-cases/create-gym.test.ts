25ac47b3bef98a27a19bedc5d12094c097228485
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-12-14 07:12:09 -0300

    feat: create gym use case and its tests

---
+++ src/use-cases/create-gym.test.ts
@@ -0,0 +1,26 @@
+import { InMemoryGymRepository } from '@/repositories/in-memory/in-memory-gym-repository'
+import { it, expect, describe, beforeEach } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let usersRepository: InMemoryGymRepository
+// authenticateUseCase: name the main entity or variable we are testing as sut (Sistem under Test) - Desing pattern
+let sut: CreateGymUseCase
+
+describe('Register use case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryGymRepository()
+    sut = new CreateGymUseCase(usersRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JS gym',
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
