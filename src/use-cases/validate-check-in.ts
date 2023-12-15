c2eec2f96e5f185ca48fe78616b6e244a8aa02b9
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-12-15 09:31:29 -0300

    feat: create Validate Check In Use Case

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,39 @@
+import { CheckInRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
+
+interface ValidateValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInRepository: CheckInRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInRepository.findById(checkInId)
+
+    if (!checkIn) throw new ResourceNotFoundError()
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20)
+      throw new LateCheckInValidationError()
+
+    checkIn.validate_at = new Date()
+
+    await this.checkInRepository.save(checkIn)
+
+    return { checkIn }
+  }
+}
